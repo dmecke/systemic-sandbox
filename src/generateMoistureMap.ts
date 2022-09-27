e0c9ab6cf6b063171bbe90cbe2fad9f20cf19a03
Daniel Mecke <daniel.mecke@cunningsoft.de>
2022-09-27 21:02:05 +0200

    adjusted moisture map

--- src/generateMoistureMap.ts
+++ src/generateMoistureMap.ts
@@ -16,9 +16,24 @@ export default function(noise: NoiseFunction2D): number[][] {
 
 function moisture(x: number, y: number, noise: NoiseFunction2D): number {
     const size = config.generation.size;
-    const nx = x / size.x;
-    const ny = y / size.y;
-    const frequency = 2.0;
+    const nx = x / size.x - 0.5;
+    const ny = y / size.y - 0.5;
 
-    return noise(nx * frequency, ny * frequency) / 2 + 0.5;
+    let moisture = 0;
+
+    moisture += 1.00 * n(noise, 1 * nx, 1 * ny);
+    moisture += 0.50 * n(noise, 2 * nx, 2 * ny);
+    moisture += 0.25 * n(noise, 4 * nx, 4 * ny);
+    moisture += 0.13 * n(noise, 8 * nx, 8 * ny);
+    moisture += 0.06 * n(noise, 16 * nx, 16 * ny);
+    moisture += 0.03 * n(noise, 32 * nx, 32 * ny);
+
+    moisture = moisture / (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03);
+    moisture = Math.pow(moisture, 2.00);
+
+    return moisture * 1.8;
+}
+
+function n(noise, nx, ny) {
+    return noise(nx, ny) / 2 + 0.5;
 }
