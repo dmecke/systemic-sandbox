26c0a73f36043273467e30733537febe92d776f0
Daniel Mecke <daniel.mecke@cunningsoft.de>
2022-10-02 20:22:13 +0200

    refactored UpdateIsInViewport

--- src/System/UpdateIsInViewport.ts
+++ src/System/UpdateIsInViewport.ts
@@ -1,3 +1,4 @@
+import Area from '../Engine/Math/Area';
 import CameraComponent from '../Component/CameraComponent';
 import IsInViewport from '../Component/IsInViewport';
 import Position from '../Component/Position';
@@ -12,45 +13,29 @@ export default class UpdateIsInViewport extends System {
     componentsRequired = new Set<Function>([Position]);
 
     update(query: Query): void {
-        const [cameraComponent] = this.ecs.queryAll(Position, CameraComponent)[0];
-        const cameraPosition = cameraComponent.position;
+        const [positionComponent] = this.ecs.queryAll(Position, CameraComponent)[0];
+        const cameraPosition = positionComponent.position;
         const buffer = new Vector(2, 2).multiply(config.tileSize);
         const cameraTopLeft = cameraPosition.subtract(buffer);
-        const cameraBottomRight = cameraTopLeft.add(this.cameraSize).add(buffer.multiply(2));
+        const viewport = new Area(cameraTopLeft, this.cameraSize.add(buffer.multiply(2)));
 
         query
             .entities()
-            .filter(entity => this.ecs.getComponents(entity).has(IsInViewport))
-            .filter(entity => !this.isInViewport(this.ecs.getComponents(entity).get(Position).position, cameraTopLeft, cameraBottomRight))
-            .forEach(entity => this.ecs.removeComponent(entity, IsInViewport))
+            .forEach(entity => {
+                const components = this.ecs.getComponents(entity);
+                const isInViewport = viewport.contains(components.get(Position).position);
+
+                if (components.has(IsInViewport)) {
+                    if (!isInViewport) {
+                        this.ecs.removeComponent(entity, IsInViewport);
+                    }
+                } else {
+                    if (isInViewport) {
+                        this.ecs.addComponent(entity, new IsInViewport());
+                    }
+                }
+            })
         ;
-
-        query
-            .entities()
-            .filter(entity => this.isInViewport(this.ecs.getComponents(entity).get(Position).position, cameraTopLeft, cameraBottomRight))
-            .filter(entity => !this.ecs.getComponents(entity).has(IsInViewport))
-            .forEach(entity => this.ecs.addComponent(entity, new IsInViewport()))
-        ;
-    }
-
-    private isInViewport(position: Vector, cameraTopLeft: Vector, cameraBottomRight: Vector): boolean {
-        if (position.x < cameraTopLeft.x) {
-            return false;
-        }
-
-        if (position.x > cameraBottomRight.x) {
-            return false;
-        }
-
-        if (position.y < cameraTopLeft.y) {
-            return false;
-        }
-
-        if (position.y > cameraBottomRight.y) {
-            return false;
-        }
-
-        return true;
     }
 
     private get cameraSize(): Vector {
